96e29f29f897f16f5f158d0733616607a0c611d5
belenenem <belenenemark@gmail.com>
2020-10-24 18:30:35 -0300

    commit inicial

---
+++ AgendaViaje/src/main/java/com/example/demo/AgendaViajeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaViajeApplication.class, args);
+	}
+
+}

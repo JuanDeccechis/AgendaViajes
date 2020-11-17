99d0ada4913f744132761ee81f7501e456d5aa96
mateo-zarrabeitia <mateo.zarrabeitia96@gmail.com>
2020-11-17 13:10:32 -0300

    Cambios de nombres en el formato de salida de la api viajes

--- agenda-microservicio-viaje/src/main/java/edu/unicen/servicio/viaje/model/Alojamiento.java
+++ agenda-microservicio-viaje/src/main/java/edu/unicen/servicio/viaje/model/Alojamiento.java
@@ -6,6 +6,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -18,17 +21,24 @@ public class Alojamiento extends Plan {
 	private static final long serialVersionUID = -5761152425914128175L;
 	
 	@Column 
+	@JsonProperty("Tipo Alojamiento")
 	private String tipo_alojamiento;
 	@Column
+	@JsonProperty("Direccion")
 	private String direccion;
 	
 	@Column
+	@JsonProperty("Localidad")
 	private String localidad;
 	
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	@Column(nullable = true)
+	@JsonProperty("Nro. Habitacion")
 	private String numero_habitacion;
 	
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	@Column(nullable = true)
+	@JsonProperty("Cod. Reserva")
 	private String codigo_reserva;
 	
 

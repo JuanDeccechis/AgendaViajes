7f599a2e20700406316e5fec5858dc037077d29a
mateo-zarrabeitia <mateo.zarrabeitia96@gmail.com>
2020-11-24 11:22:39 -0300

    Bug

--- agenda-microservicio-usuario/src/test/java/edu/unicen/servicio/usuario/controller/UsuarioControllerJPATest2.java
+++ agenda-microservicio-usuario/src/test/java/edu/unicen/servicio/usuario/controller/UsuarioControllerJPATest2.java
@@ -9,7 +9,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
